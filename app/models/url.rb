b13b5c2f09e46eb33c9c412f2bbff023f01d6f5f
Ryan <YOUR@EMAIL.com>
2017-12-22 18:34:31 +0800

    change regex for long_url validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,7 +3,7 @@ require 'securerandom'
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
 	before_create :shorten
-	validates :long_url, format: { with: /(http\:\/\/)?(https\:\/\/)?.+/, message: "Invalid URL!"}
+	validates :long_url, format: { with: /\A(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?\z/ix, message: "Invalid URL!"}
 
 	def shorten
 		self.short_url = SecureRandom.urlsafe_base64(4)
